8af1a70b02d49552c670ba5a39cde6308d88cb6d
Suyai Slootmans <suyaislootmans@gmail.com>
2021-08-13 12:24:32 -0300

    emitir mensaje

--- sockets/socket.js
+++ sockets/socket.js
@@ -2,12 +2,20 @@ const { io } = require('../index');
 
 //Mensajes de Sockets
 io.on('connection', client => {
+
     console.log('Cliente conectado');
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
     });
+
     client.on('mensaje', (payload) => {
         console.log('Mensaje!!!', payload);
         io.emit('mensaje', { admin: 'Nuevo mensaje' });
-    })
+    });
+
+    client.on('emitir-mensaje', (payload) => {
+       //io.emit('nuevo-mensaje', payload); -- emite a todos;
+        client.broadcast.emit('nuevo-mensaje', payload); //emite a todos menos a el que lo emitió;
+    });
+
 });
