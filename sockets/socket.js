86d4cfe81f01df898508ba8b446056728c7a7c7b
Suyai Slootmans <suyaislootmans@gmail.com>
2021-08-17 11:58:42 -0300

    socket: Emitir bandas registradas

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,22 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Metallica'));
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Pescado rabioso'));
+bands.addBand(new Band('v8'));
+
 
 //Mensajes de Sockets
 io.on('connection', client => {
 
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
+
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
     });
@@ -14,8 +27,8 @@ io.on('connection', client => {
     });
 
     client.on('emitir-mensaje', (payload) => {
-       
-       //io.emit('nuevo-mensaje', payload); -- emite a todos;
+
+        //io.emit('nuevo-mensaje', payload); -- emite a todos;
         client.broadcast.emit('nuevo-mensaje', payload); //emite a todos menos a el que lo emitió;
     });
 
